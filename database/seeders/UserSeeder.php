44cb282664bd67d6eca9feb97fa577cdb907a15b
Jorge Edo. Hernández <jorge@galpha.co>
2021-03-04 14:01:05 -0300

    Seeder User

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'      =>  'Admin',
+            'email'     =>  'admin@admin.com',
+            'password'  =>   Hash::make('Admin')
+        ]);
+    }
+}
